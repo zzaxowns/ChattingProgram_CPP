9afbee3659df311ea5b23addeeee97a45bc562e0
KimTaeJun <zzaxowns@naver.com>
2021-11-07 21:09:11 +0900

    채팅 창 디자인
    채팅 창 제작

--- Socket Chatting Application/Socket Chatting Application/MyForm.h
+++ Socket Chatting Application/Socket Chatting Application/MyForm.h
@@ -34,6 +34,11 @@ namespace SocketChattingApplication {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::RichTextBox^  richTextBox1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +53,68 @@ namespace SocketChattingApplication {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->richTextBox1 = (gcnew System::Windows::Forms::RichTextBox());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 25);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(42, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"IP 林家";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(290, 20);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"立加";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(60, 22);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(210, 20);
+			this->textBox1->TabIndex = 2;
+			// 
+			// richTextBox1
+			// 
+			this->richTextBox1->Location = System::Drawing::Point(15, 62);
+			this->richTextBox1->Name = L"richTextBox1";
+			this->richTextBox1->Size = System::Drawing::Size(350, 209);
+			this->richTextBox1->TabIndex = 3;
+			this->richTextBox1->Text = L"";
+			this->richTextBox1->TextChanged += gcnew System::EventHandler(this, &MyForm::richTextBox1_TextChanged);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(377, 283);
+			this->Controls->Add(this->richTextBox1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+
+	}
+	private: System::Void richTextBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+
+	}
 	};
 }
